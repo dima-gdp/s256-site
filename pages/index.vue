3eed374c38da36aae54671920fc21c533f8cb393
DmitryGolubev <dmitrii.golubev@s256.ru>
2022-07-04 16:52:26 +0300

    Добавлен 3 блок

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,17 @@
   <main ref="main" class="main-page">
     <HeroSection />
     <UseSection />
+    <SpecializationSection />
   </main>
 </template>
 
 <script>
 import HeroSection from '~/components/sections/hero/hero-section'
 import UseSection from '~/components/sections/use/use-section'
+import SpecializationSection from '~/components/sections/specialization/specialization-section'
 
 export default {
-  components: { HeroSection, UseSection },
+  components: { HeroSection, UseSection, SpecializationSection },
   data() {
     return {}
   },
@@ -41,5 +43,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~assets/scss/pages/main-page";
+@import '~assets/scss/pages/main-page';
 </style>
