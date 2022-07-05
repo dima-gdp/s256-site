328784bd02010e2ff51cd036eae16a28c2a7afbb
chudowinter <chudowinter@gmail.com>
2022-07-05 15:28:32 +0300

    feat: WEB-20: Добавлен компонент компетенций

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,9 +1,27 @@
 import IconMouse from './icon-mouse.vue'
 import IconChevronRight from './icon-chevron-right.vue'
 import IconText from './icon-text.vue'
+import IconAnalytics from './icon-analytics.vue'
+import IconDesign from './icon-design.vue'
+import IconSoftware from './icon-software.vue'
+import IconTesting from './icon-testing.vue'
+import IconSupport from './icon-support.vue'
+import IconInstrumentConnection from './icon-instrument-connection.vue'
+import IconArrows from './icon-arrows.vue'
+import IconMarketing from './icon-marketing.vue'
+import IconTypesOfIntegrations from './icon-types-of-integrations.vue'
 
 export {
   IconMouse,
   IconChevronRight,
-  IconText
+  IconText,
+  IconAnalytics,
+  IconDesign,
+  IconSoftware,
+  IconTesting,
+  IconSupport,
+  IconInstrumentConnection,
+  IconArrows,
+  IconMarketing,
+  IconTypesOfIntegrations,
 }
