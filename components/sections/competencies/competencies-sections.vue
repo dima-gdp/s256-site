99774caaee83f1d674b4869c60a0b03d8126012e
chudowinter <chudowinter@gmail.com>
2022-07-07 11:02:11 +0300

    feat: WEB-20: Добавлен компонент компетенций

---
+++ components/sections/competencies/competencies-sections.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="competencies container padding-top padding-bottom">
+    <div class="competencies__container">
+      <div class="competencies__content">
+        <div class="competencies__section">
+            <IconAnalytics class="competencies__icon" />
+          <span class="competencies__text">Аналитика и проектирование</span>
+        </div>
+        <div class="competencies__section">
+            <IconDesign class="competencies__icon" />
+          <span class="competencies__text">Продуктовый дизайн и айдентика</span>
+        </div>
+        <div class="competencies__section">
+            <IconSoftware class="competencies__icon" />
+          <span class="competencies__text">
+            Разработка программного обеспечения
+          </span>
+        </div>
+        <div class="competencies__section">
+            <IconTesting class="competencies__icon" />
+          <span class="competencies__text">
+            Тестирование и контроль качества
+          </span>
+        </div>
+        <div class="competencies__section">
+            <IconSupport class="competencies__icon" />
+          <span class="competencies__text">
+            Поддержка и развитие продукта
+          </span>
+        </div>
+      </div>
+
+      <div class="competencies__box">
+        <h2 class="competencies__title text-large">Компетенции</h2>
+      </div>
+
+      <div class="competencies__content">
+        <div class="competencies__section">
+            <IconInstrumentConnection  class="competencies__icon"/>
+          <span class="competencies__text"> Подключение BI&nbsp;-&nbsp;инструментов </span>
+        </div>
+        <div class="competencies__section">
+          <IconArrows class="competencies__icon" />
+          <span class="competencies__text">
+            Переработка&nbsp;проекта&nbsp;</span
+          >
+        </div>
+        <div class="competencies__section">
+          <IconMarketing class="competencies__icon" />
+          <span class="competencies__text"> Маркетинг и продвижение </span>
+        </div>
+        <div class="competencies__section integrations">
+          <IconTypesOfIntegrations class="competencies__icon integrations-icon" />
+          <span class="competencies__text">
+            Все виды интеграций: платёжные системы, рассылки, кассы, CRM и
+            ERP-системы, интеграции 1С
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  IconAnalytics,
+  IconDesign,
+  IconSoftware,
+  IconTesting,
+  IconSupport,
+  IconInstrumentConnection,
+  IconArrows,
+  IconMarketing,
+  IconTypesOfIntegrations,
+} from '~/components/icons'
+export default {
+  components: {
+    IconAnalytics,
+    IconDesign,
+    IconSoftware,
+    IconTesting,
+    IconSupport,
+    IconInstrumentConnection,
+    IconArrows,
+    IconMarketing,
+    IconTypesOfIntegrations,
+  },
+}
+</script>
+<style lang="scss">
+@import 'competencies-sections';
+</style>
